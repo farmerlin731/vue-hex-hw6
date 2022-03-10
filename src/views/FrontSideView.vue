2a8f5bd6cf6561689e167f1887d061217f15ac5b
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2022-03-10 18:33:09 +0800

    test-push-cli-ver2

--- src/views/FrontSideView.vue
+++ src/views/FrontSideView.vue
@@ -4,7 +4,7 @@
     <div id="nav">
       <router-link to="/">首頁</router-link> |
       <router-link to="/products">前台產品列表</router-link> |
-      <router-link to="/cart">前台購物車</router-link> | Ver1
+      <router-link to="/cart">前台購物車</router-link> | Ver2
     </div>
   </div>
   <router-view></router-view>
